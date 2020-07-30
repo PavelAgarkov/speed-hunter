69a5fcb742b882953356b886be833307d11b9946
PavelAgarkov <agarkov.ru@mail.ru>
2020-07-30 14:39:12 +0300

    Рабочий прототип с использованием shared memory

--- index.php
+++ index.php
@@ -1,52 +1,44 @@
 <?php
+
 // non-blocking-proc_open.php
 // File descriptors for each subprocess.
+
+$login = posix_getuid();
 
 $start = microtime(true);
 
 //--------------------------------------
 
-$MEMSIZE = 512; //  объём выделяемой разделяемой памяти
-$SEMKEY = 1;   //  ключ семафора
-$SHMKEY = 2;   //  ключ разделяемой памяти
-
-echo "Старт.\n";
-
-// Создаем семафор
-$sem_id = sem_get($SEMKEY, 1);
-if ($sem_id === false)
-{
-    echo "Ошибка при создании семафора";
-    exit;
-}
-else
-    echo "Создан семафор $sem_id.\n";
-
-//--------------------------------------
-
-// Занимаем семафор
-if (! sem_acquire($sem_id))
-{
-    echo "Ошибка при попытке занять семафор $sem_id.\n";
-    sem_remove($sem_id);
-    exit;
-}
-else
-    echo "Успешно занят семафор $sem_id.\n";
+$sharedMemorySize = 1000000; //  объём выделяемой разделяемой памяти
+$streamPool = [];
+$output = [];
 
 //--------------------------------------
 
 // Подключаем разделяемую память
-$shm_id = shm_attach($SHMKEY, $MEMSIZE);
 
-if ($shm_id === false)
-{
-    echo "Ошибка при подключении разделяемой памяти.\n";
-    sem_remove($sem_id);
-    exit;
+while (count($streamPool) < 5) {
+    //  ключ разделяемой памяти
+    $sharedMemoryKey = rand(100, 1000000);
+    $sharedMemoryId = shmop_open($sharedMemoryKey, "n", 0755, $sharedMemorySize);
+
+    // если разделяемая память по данному ключу занята, то делаем новый ключ
+    if ($sharedMemoryId === false) {
+        echo "Ошибка при подключении разделяемой памяти.\n";
+        $sharedMemoryKey = rand(100, 1000000);
+        // иначе записываем в пул памяти
+    } else {
+        $memory = (string)$sharedMemoryId;
+        $memoryNumber = preg_replace('/[^0-9]/', '', explode(' ', $memory)[2]);
+        $streamPool[][$memoryNumber] = [
+            $sharedMemoryId,
+            $sharedMemoryKey
+        ];
+    }
 }
-else
-    echo "Успешное подключение разделяемой памяти: $shm_id.\n";
+
+
+echo "Старт.\n";
 
 //--------------------------------------
 
@@ -55,38 +47,22 @@ $descriptors = [
     1 => ['pipe', 'w'], // stdout,
     2 => array("file", "/tmp/error-output.txt", "a")
 ];
-
 $pipes = [];
 $processes = [];
-$memory = (string) $shm_id;
-$semofore = (string) $sem_id;
+$procPipes = [];
 
-$memoryNumber = preg_replace('/[^0-9]/', '', explode(' ', $memory)[2]);
-$semoforeNumber = preg_replace('/[^0-9]/', '', explode(' ', $semofore)[2]);
-
-//if (!shm_put_var($shm_id, 1, [0 => $shm_id, 1 => $sem_id]))
-//{
-//    echo "Ошибка при попытке записать переменную 1 в разделяемую память $shm_id.\n";
-//
-//    // Овобождаем ресурсы.
-//    sem_remove($sem_id);
-//    shm_remove($shm_id);
-//    exit;
-//}
-//else
-//    echo "Переменная 1 записана в разделяемую память.\n";
-
-
-
-foreach (range(1, 5) as $i) {
+foreach (range(0, 4) as $key => $i) {
     // Spawn a subprocess.
 
     echo "Fork $i process <br>";
+    $numberMemoryKey = current($streamPool[$key])[1];
 
-//    $proc = proc_open('php subprocess.php ' . $i . ' ' . $shm_id . ' ' . $sem_id, $descriptors, $procPipes);
-    $proc = proc_open("php subprocess.php {$i} {$memoryNumber} {$semoforeNumber}" , $descriptors, $procPipes);
+//    $proc = proc_open('php subprocess.php ' . $i . ' ' . $sharedMemoryId . ' ' . $sem_id, $descriptors, $procPipes);
+//    $proc = proc_open("php subprocess.php {$i} {$memoryNumber} {$semoforeNumber}" , $descriptors, $procPipes);
+//    $proc = proc_open("php subprocess.php {$i} {$memoryNumber}", $descriptors, $procPipes);
+    $proc = proc_open("php subprocess.php {$i} {$numberMemoryKey}", $descriptors, $procPipes);
+
     $processes[$i] = $proc;
-    $res = get_resources();
 
     // Make the subprocess non-blocking (only output pipe).
     stream_set_blocking($procPipes[1], 0);
@@ -97,127 +73,61 @@ foreach (range(1, 5) as $i) {
 
 
 // Run in a loop until all subprocesses finish.
-while (array_filter($processes, function ($proc) {
-    return proc_get_status($proc)['running'];
-})) {
-    foreach (range(1, 1) as $i) {
-//        usleep(10 * 1000); // 100ms
-        // Read all available output (unread output is buffered).
-
-        $str = fread($pipes[$i][1], 8192);
-
-        if ($str) {
-            printf($str);
-        }
-    }
-}
-
-
+//while (array_filter($processes, function ($proc) {
+//    return proc_get_status($proc)['running'];
+//})) {
+//    foreach (range(1, 1) as $i) {
+//        // Read all available output (unread output is buffered).
+//
+//        $str = fread($pipes[$i][1], 8192);
+//
+//        if ($str) {
+//            printf($str);
+//        }
+//    }
+//}
 
 
 // Close all pipes and processes.
-foreach (range(1, 5) as $i) {
+foreach (range(0, 4) as $i) {
     echo "Close output process from piping $i <br>";
     fclose($pipes[$i][1]);
     proc_close($processes[$i]);
 }
 
 // читаем что записали в память в воркере
+foreach (range(0, 4) as $key => $value) {
+    $numberMemoryKey = current($streamPool[$key])[0];
+    echo "<br>" . $numberMemoryKey . "<br>";
+//    $stream = shmop_open($numberMemoryKey, "a", 0755, 20000);
+    $read = shmop_read($numberMemoryKey, 0, shmop_size($numberMemoryKey));
+    $unserialise = unserialize($read);
+//    if ($read === false) {
+//        echo '<br>' . 'при чтении произлошла ошибка' . '<br>';
+//    }
 
-foreach (range(1, 5) as $key => $value) {
-    $a = shm_get_var($shm_id, $value);
-    print_r($a);
+    $output[] = $unserialise;
 }
 
 //--------------------------------------
 
-// Освобождаем семафор
-if (!sem_release($sem_id))
-    echo "Ошибка при попытке освободить семафор $sem_id.\n";
-else
-    echo "Семафор $sem_id освобожден.\n";
-
 // Удаляем сегмент разделяемой памяти
-if (shm_remove ($shm_id))
-    echo "Сегмент разделяемой памяти успешно удален.\n";
-else
-    echo "Ошибка при попытке удалить сегмент разделяемой памяти $shm_id.\n";
-
-// Удаляем семафор.
-if (sem_remove($sem_id))
-    echo "Семафор успешно удален.\n";
-else
-    echo "Ошибка при попытке удалить семафор $sem_id.\n";
-
-echo "Конец.\n";
+foreach (range(0, 4) as $key => $value) {
+    $numberMemoryKey = current($streamPool[$key])[0];
+    $delete = shmop_delete($numberMemoryKey);
+//    if ($delete === true) {
+//        echo PHP_EOL. 'удалось удалить сегмент памяти' . PHP_EOL;
+//    } else {
+//        echo 'не удалось' . PHP_EOL;
+//    }
+}
 
 
 //--------------------------------------
 
-
-//foreach (range(1, 5) as $i) {
-//    $ch = curl_init("https://api.github.com/repos/PavelAgarkov/pocket-typing/events");
-//    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-////    curl_setopt($ch, CURLOPT_HEADER, 1);
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//    $cont = curl_exec($ch);
-//
-//    curl_close($ch);
-//}
-
 $time = microtime(true) - $start;
 
-echo $time;
+echo $time . "<br>";
 
-
-
-
-
-
-// Пишем переменную 1
-//if (!shm_put_var($shm_id, 1, [0 => 'переменная 1']))
-//{
-//    echo "Ошибка при попытке записать переменную 1 в разделяемую память $shm_id.\n";
-//
-//    // Овобождаем ресурсы.
-//    sem_remove($sem_id);
-//    shm_remove($shm_id);
-//    exit;
-//}
-//else
-//    echo "Переменная 1 записана в разделяемую память.\n";
-//
-//// Пишем переменную 2
-//if (!shm_put_var($shm_id, 2, "Переменная 2"))
-//{
-//    echo "Ошибка при попытке записать переменную 2 в разделяемую память $shm_id.\n";
-//
-//    // Освобождаем ресурсы.
-//    sem_remove($sem_id);
-//    shm_remove ($shm_id);
-//    exit;
-//}
-//else
-//    echo "Переменная 2 записана в разделяемую память.\n";
-
-// Читаем переменную 1
-//$var1 = shm_get_var($shm_id, 1);
-//if ($var1 === false)
-//{
-//    echo "Ошибка при попытке прочитать переменную 1 из разделяемой памяти $shm_id, " .
-//        "возвращенное значение=$var1.\n";
-//}
-//else
-//    echo "Прочитана переменная 1=$var1.\n";
-//
-//// Читаем переменную 2
-//$var2 = shm_get_var ($shm_id, 2);
-//if ($var1 === false)
-//{
-//    echo "Ошибка при попытке прочитать переменную 2 из разделяемой памяти $shm_id, " .
-//        "возвращенное значение=$var2.\n";
-//}
-//else
-//    echo "Прочитана переменная 2=$var2.\n";
-
+print_r($output);
 
--- subprocess.php
+++ subprocess.php
@@ -1,11 +1,10 @@
 <?php
-$res = get_resources();
+$keyMemory = $argv[2];
 
-$smt = shm_attach($argv[2]);
+$stream = shmop_open($keyMemory, "w", 0755, 1000000);
 $name = $argv[1];
-//$shm_id = $argv[2];
 
-printf("{$name} hi <br>");
+$cont = null;
 
 $ch = curl_init("https://api.github.com/repos/PavelAgarkov/pocket-typing/events");
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36');
@@ -13,28 +12,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $cont = curl_exec($ch);
 curl_close($ch);
 
+$decode = json_decode($cont, true);
+$serialize = serialize($decode);
 
-if (!shm_put_var($smt, $name, [0 => $name]))
-{
-    echo "Ошибка при попытке записать переменную 1 в разделяемую память $shm_id.\n";
-
-    // Овобождаем ресурсы.
-    sem_remove($sem_id);
-    shm_remove($shm_id);
-    exit;
-}
-else
-    printf("Переменная 1 записана в разделяемую память <br>");
-
-//$str_utf8 = $cont;
-//$end = strlen($str_utf8);
-//$limit = 8000;
-//
-//for ($start = 0; $start <= $end; $start += $limit) {
-//    $continue = $start + $limit;
-//
-//    if($continue > $limit) $continue = null;
-//
-//    $str_utf8_0 = mb_strcut($str_utf8, $start, $continue, "UTF-8");
-//    printf($str_utf8_0);
-//}
+$write = shmop_write($stream, "{$serialize}", 0);
+exit();
